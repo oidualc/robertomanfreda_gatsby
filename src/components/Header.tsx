5c42731d7577837fc32c1f224e33938aabbfc4ad
Claudio Cocciarelli <claudiococciarelli@gmail.com>
2021-12-02 23:13:09 +0100

    feat(header): scroll animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HeaderNavLink from "./HeaderNavLink";
 import HamburgerMenuBtn from "./HeaderHamburgerMenuBtn";
 
+
 const Header = () => {
   const [open, setOpen] = useState(false);
+  const [compact, setCompact] = useState(false);
+
+  const handleScroll = () => setCompact(window.scrollY > 70);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [window.scrollY]);
 
   return (
     <header className="z-10 fixed top-0 w-full bg-black bg-opacity-90">
-      <div className="relative flex flex-col md:flex-row items-start md:items-center justify-between w-4/5 md:w-11/12 lg:w-4/5 max-w-6xl mx-auto my-6">
+      <div
+        className={`relative flex flex-col md:flex-row items-start md:items-center justify-between w-4/5 md:w-11/12 lg:w-4/5 max-w-6xl mx-auto transition-all duration-500 ${
+          compact ? "my-2" : "my-6"
+        }`}
+      >
         <h1 className="font-hacked text-3xl lg:text-4xl whitespace-nowrap">
           ROBERTO MANFREDA
         </h1>
