1c93542a7682999f5585bb0606bdf1abbe0f5013
Claudio Cocciarelli <claudiococciarelli@gmail.com>
2021-12-11 14:07:19 +0100

    fix(header): correct background small size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import HeaderNavLink from "./HeaderNavLink";
 import HamburgerMenuBtn from "./HeaderHamburgerMenuBtn";
 
-
 const Header = () => {
   const [open, setOpen] = useState(false);
   const [compact, setCompact] = useState(false);
@@ -17,27 +16,31 @@ const Header = () => {
   return (
     <header className="z-10 fixed top-0 w-full bg-black bg-opacity-90">
       <div
-        className={`relative flex flex-col md:flex-row items-start md:items-center justify-between w-4/5 md:w-11/12 lg:w-4/5 max-w-6xl mx-auto transition-all duration-500 ${
+        className={`relative flex flex-col md:flex-row items-start md:items-center justify-between md:w-11/12 lg:w-4/5 max-w-6xl mx-auto transition-all duration-500 ${
           compact ? "my-2" : "my-6"
         }`}
       >
-        <h1 className="font-hacked text-3xl lg:text-4xl whitespace-nowrap">
-          ROBERTO MANFREDA
-        </h1>
-        <span className="absolute top-0 right-0 md:hidden">
-          <HamburgerMenuBtn open={open} onClick={() => setOpen(!open)} />
-        </span>
+        <div className="relative w-4/5 mx-auto">
+          <h1 className="font-hacked text-3xl lg:text-4xl whitespace-nowrap">
+            ROBERTO MANFREDA
+          </h1>
+          <span className="absolute -top-0.5 right-0 md:hidden">
+            <HamburgerMenuBtn open={open} onClick={() => setOpen(!open)} />
+          </span>
+        </div>
 
-        <nav
-          className={`absolute md:static top-12 md:top-auto bg-black w-full md:w-auto bg-opacity-90 flex flex-col md:flex-row transition md:transition-none md:transform-none origin-top duration-500 ${
+        <div
+          className={`absolute md:static md:top-auto top-11 w-full bg-black bg-opacity-90 md:transform-none transition md:transition-none origin-top duration-500 ${
             !open ? "scale-y-0 opacity-0 md:opacity-100" : ""
           }`}
         >
-          <HeaderNavLink to="/#about" label="ABOUT" />
-          <HeaderNavLink to="/#projects" label="PROJECTS" />
-          <HeaderNavLink to="/#main-interests" label="TECNHOLOGIES" />
-          <HeaderNavLink to="/#contact" label="CONTACT" />
-        </nav>
+          <nav className="w-4/5 md:w-auto mx-auto flex flex-col md:flex-row transition duration-500">
+            <HeaderNavLink to="/#about" label="ABOUT" />
+            <HeaderNavLink to="/#projects" label="PROJECTS" />
+            <HeaderNavLink to="/#main-interests" label="TECNHOLOGIES" />
+            <HeaderNavLink to="/#contact" label="CONTACT" />
+          </nav>
+        </div>
       </div>
     </header>
   );
--- src/components/HeaderHamburgerMenuBtn.tsx
+++ src/components/HeaderHamburgerMenuBtn.tsx
@@ -8,29 +8,27 @@ type HamburgerMenuBtnProps = {
 const HamburgerMenuBtn = ({ open, onClick }: HamburgerMenuBtnProps) => {
   return (
     <button
-      className="text-white w-10 h-10 relative focus:outline-none"
+      className="relative text-white w-6 h-6 focus:outline-none"
       onClick={onClick}
     >
-      <div className="block w-5 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
-        <span
-          aria-hidden="true"
-          className={`block absolute h-0.5 w-6 bg-current transition duration-500 ease-in-out ${
-            open ? "rotate-45" : "-translate-y-1.5"
-          }`}
-        ></span>
-        <span
-          aria-hidden="true"
-          className={`block absolute h-0.5 w-6 bg-current transition duration-500 ease-in-out ${
-            open ? "opacity-0" : ""
-          }`}
-        ></span>
-        <span
-          aria-hidden="true"
-          className={`block absolute h-0.5 w-6 bg-current transition duration-500 ease-in-out ${
-            open ? "-rotate-45" : "translate-y-1.5"
-          }`}
-        ></span>
-      </div>
+      <span
+        aria-hidden="true"
+        className={`absolute block h-0.5 w-6 bg-current transition duration-500 ease-in-out ${
+          open ? "rotate-45" : "-translate-y-1.5"
+        }`}
+      ></span>
+      <span
+        aria-hidden="true"
+        className={`absolute block h-0.5 w-6 bg-current transition duration-500 ease-in-out ${
+          open ? "opacity-0" : ""
+        }`}
+      ></span>
+      <span
+        aria-hidden="true"
+        className={`absolute block h-0.5 w-6 bg-current transition duration-500 ease-in-out ${
+          open ? "-rotate-45" : "translate-y-1.5"
+        }`}
+      ></span>
     </button>
   );
 };
